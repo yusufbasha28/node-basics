a397ded660c000db079bdda2aa363825da4d51cd
yusufbasha28 <yusufbasha.28@gmail.com>
2021-04-15 17:50:35 +0530

    https://github.com/yusufbasha28/node_basics.git\

---
+++ app.js
@@ -0,0 +1,27 @@
+//functions
+
+function hello (name) {
+    console.log('hello' + name);
+}
+
+hello('yusuf');
+
+//global object
+
+// setTimeout() //delay time
+// clearTimeout()
+
+//setinterval()  // repetedly occur
+//clearinterval()
+
+var globalmessage = 'hi';
+console.log(globalmessage);
+
+//modules
+//load a module
+
+console.log(module);
+//load a module
+var log = require('./logger').default;
+
+console.log('message');
---
+++ logger.js
@@ -0,0 +1,10 @@
+console.log(___filename);
+console.log(___dirname);
+ var url = 'http://mylogger.io/log';
+
+function log(message) {
+//send an http req
+
+console.log(message);
+}
+module.exports = log;
